82f9d21fc9978666a4bb0face8e2cd13f6050b96
Lao <52985243+Simon-4@users.noreply.github.com>
2020-04-20 02:37:04 -0300

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,45 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Leonardo on 19/04/20.
+//  Copyright © 2020 Leonardo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            MapView()
+                .edgesIgnoringSafeArea(.top)
+                .frame(height: 300)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack (alignment: .leading){
+                Text("Turtle Rock")
+                    .font(.title)
+                HStack {
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+
+                }
+            }
+            .padding()
+            
+            Spacer()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Landmarks/Preview Content/CircleImage.swift
@@ -0,0 +1,21 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Leonardo on 20/04/20.
+//  Copyright © 2020 Leonardo. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image(turtlelock)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
